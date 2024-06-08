f696609e2cbf4446a002dc0c98b5f72e1779beaa
jzubietas <jhonathanisaizubietasantos@gmail.com>
2024-06-08 00:52:32 -0500

    tv

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
     <footer class="footer text-center text-sm-start">
-            &copy; 2021 Dastone <span class="d-none d-sm-inline-block float-end">Crafted with <i class="mdi mdi-heart text-danger"></i> by Mannatthemes</span>
+            &copy; 2024 Dastone <span class="d-none d-sm-inline-block float-end">Crafted with <i class="mdi mdi-heart text-danger"></i> by TV</span>
     </footer><!--end footer-->
 
 
