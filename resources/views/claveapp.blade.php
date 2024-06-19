40e2446dace7896d2b69d1ef4d0d08d68700a912
jzubietas <jhonathanisaizubietasantos@gmail.com>
2024-06-19 12:05:08 -0500

    abc

--- resources/views/claveapp.blade.php
+++ resources/views/claveapp.blade.php
@@ -53,43 +53,43 @@
                                             </div>
                                         </div>
 
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-email-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Email</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-tel-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Telephone</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="tel" value="1-(555)-555-5555" id="example-tel-input">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-password-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Password</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="password" value="hunter2" id="example-password-input">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-number-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Number</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="number" value="42" id="example-number-input">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-datetime-local-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Date and time</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="exampleColorInput" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Color</label>
                                             <div class="col-sm-10">
                                                 <input type="color" class="form-control form-control-color" id="exampleColorInput" value="#0b51b7" title="Choose your color">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Select</label>
                                             <div class="col-sm-10">
                                                 <select class="form-select" aria-label="Default select example">
@@ -100,19 +100,19 @@
                                                     </select>
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-text-input-lg" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Large</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" id="example-text-input-lg">
                                             </div>
                                         </div>
-                                        <div class="mb-3 row">
+                                        <div class="mb-3 row d-none">
                                             <label for="example-text-input-sm" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Small</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" id="example-text-input-sm">
                                             </div>
                                         </div>
-                                        <div class="mb-3 mb-lg-0 row">
+                                        <div class="mb-3 mb-lg-0 row d-none">
                                             <label for="example-search-input" class="col-sm-2 form-label align-self-center mb-lg-0 text-end">Search</label>
                                             <div class="col-sm-10">
                                                 <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
