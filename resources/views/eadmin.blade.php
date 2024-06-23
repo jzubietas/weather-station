bc330d2729aa266fbdfee3dd848e24fccf9a8118
jzubietas <jhonathanisaizubietasantos@gmail.com>
2024-06-23 13:13:50 -0500

    asd

--- resources/views/eadmin.blade.php
+++ resources/views/eadmin.blade.php
@@ -848,6 +848,7 @@
 @endsection
 @section('script')
     <script src="{{ URL::asset('assets/plugins/huebee/huebee.pkgd.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/pages/jquery.forms-advanced.js') }}"></script>
     <script src="{{ URL::asset('assets/js/app.js') }}"></script>
 @endsection
 
